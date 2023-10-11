5e8fdf5da30e2e11c9bc2615d30a0cbc9e064f69
Sude Mengen <smengen@ogr.iu.edu.tr>
2023-10-11 19:37:02 +0300

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Sude Mengen",
+  title: "Business Administration Student at Istanbul University & Alpha MLSA",
+  email: "sudemengen@studentambassadors.com",
   gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  instagram: "sudemngn",
+  linkedIn: "sudemengen",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portfolyokismi.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "my desktop";
 
 /**
  * Project list
@@ -31,22 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "If you want to follow my career:",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "I regularly share what I do on my LinkedIn account.",
+    url: "https://www.linkedin.com/in/sudemengen/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Everything I do, I do for myself",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "My favorite thing in my life is investing in myself.",
+    url:"https://www.linkedin.com/in/sudemengen/",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "You can go to my GitHub account and check out the resources for the site I built for MLSA.",
+    url: "https://github.com/sudemngn",
   },
   {
     title: "GitHub Codespaces and github.dev",
